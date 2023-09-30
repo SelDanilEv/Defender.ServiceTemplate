361db1d56e65075ff3e20365af9e8e4224975746
Danil <47692381+SelDanilEv@users.noreply.github.com>
2023-09-30 21:57:18 +0400

    Add secrets file

--- src/dockerDeployment.bat
+++ src/dockerDeployment.bat
@@ -1,6 +1,7 @@
 docker rm -f ServiceTemplate
 docker build . -t service-template && ^
 docker run -d --name ServiceTemplate -p 2500:80 ^
+--env-file ./../../secrets.list ^
 -e ASPNETCORE_ENVIRONMENT=DockerDev ^
 -it service-template
 echo finish service-template
